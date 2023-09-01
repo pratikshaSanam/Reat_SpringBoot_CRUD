7914b572a0d291098c89a5041da31d7888ecf9d0
pratikshaSanam <pratikshasanam@gmail.com>
2023-09-01 18:18:29 +0530

    react and spring boot crud application

---
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.fullstackbackend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+	
+	public UserNotFoundException(Long id) {
+		
+		super("could not  found  the user"+id);
+	}
+
+}
---
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.fullstackbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.fullstackbackend.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{ 
+	
+	
+
+}
